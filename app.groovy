cfe607a091cfbc505d2e0be95ad1143d072b2320
Greg Turnquist <gturnquist@gopivotal.com>
2014-05-08 10:11:07 -0400

    Add a basic detector
    Initialize the MessageEndpoint with `latest` and as each new row comes in, update it. Then have a scheduled task check if the diff between `latest` and `now` exceeds a threshold.

--- app.groovy
+++ app.groovy
@@ -1,9 +1,12 @@
 package com.greglturnquist.springmonitor
 
+import groovy.util.logging.*
+import org.joda.time.*
 import org.springframework.integration.file.tail.OSDelegatingFileTailingMessageProducer
 
 @Grab("org.springframework.integration:spring-integration-java-dsl:1.0.0.BUILD-SNAPSHOT")
-@Log
+@Grab("joda-time:joda-time:2.3")
+@Slf4j
 @Configuration
 @EnableIntegrationPatterns
 class Monitor {
@@ -23,12 +26,28 @@ class Monitor {
     
 }
 
-@Log
+@Slf4j
 @MessageEndpoint
+@EnableScheduling
 class MonitorService {
+
+    DateTime latest = new DateTime()
 
     @ServiceActivator(inputChannel = "tailChannel")
     void handle(String data) {
-        log.info(data)
+        if (data.contains("LightningData")) {
+            latest = new DateTime(Date.parse("yyyy-MM-dd HH:mm:ss.SSS", data[0..22]))
+        }
+    }
+    
+    @Scheduled(fixedRate = 5000L)
+    void check() {
+        def now = new DateTime()
+        def duration = new Duration(latest, now)
+        if (duration.toStandardSeconds().seconds >= 10) {
+            log.error("It has been ${duration.toStandardSeconds().seconds} seconds since last update!")
+        } else {
+            log.info("It has been ${duration.toStandardSeconds().seconds} seconds since last update")
+        }
     }
 }
